1b9bb1bcada2ab761eebbbcf1995d2219e9aeac8
abdom11 <abd123abdo.com@gmail.com>
2023-09-13 15:31:58 +0300

    good progress

--- Server/server.js
+++ Server/server.js
@@ -1,32 +1,41 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-const { ExpressPeerServer } = require("peer");
+const { v4: uuidV4 } = require("uuid");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Serve your static files (HTML, CSS, JS, etc.)
-const path = require("path");
-app.use(express.static(path.join(__dirname, "public")));
-
-// Set up PeerJS server
-const peerServer = ExpressPeerServer(server, {
-  debug: true,
-  path: "/peerjs", // The path to PeerJS server
-});
-
 // Middleware to handle PeerJS requests
-app.use("/peerjs", peerServer);
+app.set("view engine", "ejs"); // Tell Express we are using EJS
+app.use(express.static("views"));
 
 // Set up WebSockets for signaling
+
+// When someone connects to the server
 io.on("connection", (socket) => {
-  // Handle WebRTC signaling here
+  // When someone attempts to join the room
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId); // Join the room
+    socket.broadcast.emit("user-connected", userId); // Tell everyone else in the room that we joined
+    console.log("User Connected");
+
+    // Communicate the disconnection
+    socket.on("disconnect", () => {
+      socket.broadcast.emit("user-disconnected", userId);
+      console.log("User Disconnected");
+    });
+  });
 });
 
-app.get("/peerjs", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "home.html"));
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidV4()}`);
+});
+
+// If they join a specific room, then render that room
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
 });
 
 // Start the server
